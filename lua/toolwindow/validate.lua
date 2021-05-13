5d18c705ad62660c5e90dde3c54dfc191d68e970
EthanJWright <ewright362@gmail.com>
2021-05-13 00:26:40 -0600

    fix: watchexec install tested

--- lua/toolwindow/validate.lua
+++ lua/toolwindow/validate.lua
@@ -3,7 +3,7 @@ local M = {}
 
 function M.validate()
   if vim.fn.executable("watchexec") == 0 then
-    api.nvim_err_writeln("watchexec is not installed. Call :WatchexecInstall to install it")
+    api.nvim_err_writeln("watchexec is not installed. Call :WatchexecInstall to install it\n")
     return false
   end
   return true
@@ -11,13 +11,14 @@ end
 
 local function call_cargo_command()
   local cmd = {"cargo", "install", "watchexec-cli"}
+  print("A message will print once install is complete, it can may take up to 20 seconds...")
   vim.fn.jobstart(cmd, {
     on_exit = function(_, d, _)
       if d == 0 then
-        api.nvim_out_write("latest watchexec installed")
+        api.nvim_out_write("latest watchexec installed!\n")
         return
       end
-      api.nvim_err_writeln("failed to install watchexec")
+      api.nvim_err_writeln("failed to install watchexec\n")
     end,
   })
 end
