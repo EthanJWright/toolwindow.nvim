ee546dba564cf84687cda17d29470c714c2a69d9
EthanJWright <ewright362@gmail.com>
2021-05-12 20:49:53 -0600

    feat: first attempt at adding watchexec installer

--- lua/toolwindow/windows.lua
+++ lua/toolwindow/windows.lua
@@ -1,5 +1,8 @@
 local Terminal  = require('toggleterm.terminal').Terminal
 local Windows = {}
+local validate = require('toggleterm.validate')
+
+validate.validate()
 
 
 local function standard_close(plugin)
